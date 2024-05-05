3a38a520537dcfbe470bf75023fa2691113deb39
GavinGoo <1226683343@qq.com>
2024-05-06 02:18:05 +0800

    支持隔离用户对话; 替换Chat页面Prompt

--- bin/startup.sh
+++ bin/startup.sh
@@ -126,10 +126,6 @@ if [ -n "${PANDORA_THREADS}" ]; then
   PANDORA_ARGS="${PANDORA_ARGS} --threads ${PANDORA_THREADS}"
 fi
 
-if [ -n "${PANDORA_ISOLATION_MASTERCODE}" ]; then
-  PANDORA_ARGS="${PANDORA_ARGS} --isolate_master ${PANDORA_ISOLATION_MASTERCODE}"
-fi
-
 if [ -n "${PANDORA_CLOUD}" ]; then
   PANDORA_COMMAND="pandora-cloud"
 fi
